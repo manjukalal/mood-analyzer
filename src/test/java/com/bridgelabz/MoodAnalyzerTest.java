f9a23049f830efbf6aba96f8e710b9a10f9b7cf8
manjukalal <manjukalal1496@gmail.com>
2022-07-03 12:48:24 +0530

    Added:mood analyzer

---
+++ src/test/java/com/bridgelabz/MoodAnalyzerTest.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MoodAnalyzerTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        Assert.assertTrue(true);
+    }
+}
